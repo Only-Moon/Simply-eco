9445455c1fa26fd18202239a85a425a5e845718c
RaZe <razeslothc@gmail.com>
2021-12-23 11:59:10 +0200

    Updated typings!

--- lib/typings/index.d.ts
+++ lib/typings/index.d.ts
@@ -1,3 +1,5 @@
+import { Client } from "discord.js";
+
 declare module "simply-eco" {
 
     export type wallet = number | null;
@@ -6,7 +8,9 @@ declare module "simply-eco" {
     export type job = { Job: string };
 
     export type Item = { Name: string, Price: string, Sell: string, id: 0 | number };
-
+    export interface Options {
+        GuildID?: string;
+    }
     export interface Profile {
         user: string,
         wallet: wallet,
@@ -14,8 +18,14 @@ declare module "simply-eco" {
         bank: bankWallet,
         inventory: Item[] | [];
     }
-    export interface GuildData {
+    export interface guildData {
         gid: string | null;
+        shopItems: Item[] | [];
+        weeklyAmt: string | 10000;
+        dailyAmt: string | 2000;
+    }
+    export interface globalData {
+        Id: string | null;
         shopItems: Item[] | [];
         weeklyAmt: string | 10000;
         dailyAmt: string | 2000;
@@ -32,37 +42,37 @@ declare module "simply-eco" {
         lastUsedWork: Date;
     }
     export interface optionsInterface {
-        notify: boolean;
-        global: boolean;
+        notify?: boolean;
+        global?: boolean;
     }
 
     export class eco {
-        constructor(dbUrl: string, options?: optionsInterface);
-        public SetWeekly(GuildID: string, Amt: number): Promise<GuildData>;
-        public SetDaily(GuildID: string, Amt: number): Promise<GuildData>;
-        public Transfer(GuildID: string, User1ID: string, User2ID: string, Amt: number): Promise<{ USER1: Profile, USER2: Profile } | Profile | 'NOT_ENOUGH_CASH'>;
-        public RemoveItem(GuildID: string, ItemID: string): Promise<GuildData | 'NO_ITEMS'>;
-        public AddItem(GuildID: string, ItemName: string, Price: number, SellPrice: number): Promise<GuildData>;
-        public BuyItem(UserID: string, GuildID: string, ItemN: string): Promise<false | 'ITEM_NOT_FOUND' | 'NOT_ENOUGH_CASH' | 'ALREADY_PURCHASED' | Item>;
-        public SellItem(UserID: string, GuildID: string, ItemN: string): Promise<false | 'TRY_AGAIN' | 'NO_ITEM_IN_INVENTORY' | 'NOT_PURCHASED' | Item>;
-        public Daily(UserID: string, GuildID: string): Promise<wallet | { error: 'ALREADY_USED', timeOut: string }>;
-        public Weekly(UserID: string, GuildID: string): Promise<wallet | { error: 'ALREADY_USED', timeOut: string }>;
-        public GetInv(UserID: string, GuildID: string): Promise<Item[] | []>;
-        public GetUser(UserID: string, GuildID: string): Promise<User>;
-        public GetBal(UserID: string, GuildID: string): Promise<wallet>
-        public GetBankBal(UserID: string, GuildID: string): Promise<bankWallet>;
-        public AddMoney(UserID: string, GuildID: string, Amt: number): Promise<wallet>;
-        public RemoveMoney(UserID: string, GuildID: string, Amt: number): Promise<wallet>;
-        public GetRich(GuildID: string): Promise<"NO_RICH_PEOPLE" | User[]>;
-        public GetProfile(GuildID: string, UserID: string): Promise<Profile[]>;
-        public Deposit(UserID: string, GuildID: string, Amt: number | "max"): Promise<'CASH_IN_WALLET' | 'NO_CASH_IN_WALLET' | { wallet: wallet, Bank: bankWallet }>;
-        public Withdraw(UserID: string, GuildID: string, Amt: number | "max"): Promise<'CASH_IN_BANK' | { wallet: wallet, Bank: bankWallet }>;
-        public GetShop(GuildID: string): Promise<Item[] | []>;
-        public GetItem(guildId: string, itemName: string | number): Promise<Item | null>
-        public ReassignJob(userId: string, guildId: string, Job: string): Promise<'NOT_WORKING' | 'SUCCESS' | job[]>;
-        public RemoveJob(guildId: string, userId: string): Promise<User | "SUCCESS">;
-        public SetJob(userId: string, guildId: string, Job: string): Promise<'ALREADY_WORKING' | 'SUCCESS' | job[]>;
-        public Work(UserID: string, GuildID: string): Promise<wallet | { wallet: wallet, timeout: string, job: string, money: number }>;
+        constructor(client: Client, dbUrl: string, options?: optionsInterface);
+        public SetWeekly(options: { GuildID?: boolean, Amt: number }): Promise<guildData | globalData>;
+        public SetDaily(options: { GuildID?: boolean, Amt: number }): Promise<guildData | globalData>;
+        public Transfer(options: { GuildID?: string, User1ID: string, User2ID: string, Amt: number }): Promise<{ USER1: Profile, USER2: Profile } | Profile | 'NOT_ENOUGH_CASH'>;
+        public RemoveItem(options: { GuildID?: string, Item: string | number }): Promise<guildData | globalData | 'NO_ITEMS'>;
+        public AddItem(options: {GuildID?: string, ItemName: string, Price: number, SellPrice: number }): Promise<guildData | globalData>;
+        public BuyItem(options: {GuildID?: string, Item: string | number, UserID: string }): Promise<false | 'ITEM_NOT_FOUND' | 'NOT_ENOUGH_CASH' | 'ALREADY_PURCHASED' | Item>;
+        public SellItem(options: {GuildID?: string, Item: string | number, UserID: string }): Promise<false | 'TRY_AGAIN' | 'NO_ITEM_IN_INVENTORY' | 'NOT_PURCHASED' | Item>;
+        public Daily(options: {GuildID?: string, UserID: string }): Promise<wallet | { error: 'ALREADY_USED', timeOut: string }>;
+        public Weekly(options: {GuildID?: string, UserID: string }): Promise<wallet | { error: 'ALREADY_USED', timeOut: string }>;
+        public GetInv(options: {GuildID?: string, UserID: string }): Promise<Item[] | []>;
+        public GetUser(options: {GuildID?: string, UserID: string }): Promise<User>;
+        public GetBal(options: {GuildID?: string, UserID: string }): Promise<wallet>
+        public GetBankBal(options: {GuildID?: string, UserID: string }): Promise<bankWallet>;
+        public AddMoney(options: {GuildID?: string, Amt: number, UserID: string }): Promise<wallet>;
+        public RemoveMoney(options: {GuildID?: string, Amt: number, UserID: string }): Promise<wallet>;
+        public GetRich(options: { GuildID: string }): Promise<"NO_RICH_PEOPLE" | User[]>;
+        public GetProfile(options: {GuildID?: string, UserID: string}): Promise<Profile[]>;
+        public Deposit(options: {UserID: string, GuildID?: string, Amt: number | "max"}): Promise<'CASH_IN_WALLET' | 'NO_CASH_IN_WALLET' | { wallet: wallet, Bank: bankWallet }>;
+        public Withdraw(options: {UserID: string, GuildID?: string, Amt: number | "max"}): Promise<'CASH_IN_BANK' | { wallet: wallet, Bank: bankWallet }>;
+        public GetShop(options: { GuildID?: string}): Promise<Item[] | []>;
+        public GetItem(options: {GuildID?: string, Item: string | number }): Promise<Item | null>
+        public ReassignJob(options: { GuildID?: string, Job: string, userId: string}): Promise<'NOT_WORKING' | 'SUCCESS' | job[]>;
+        public RemoveJob(options: { GuildID?: string,  userId: string}): Promise<User | "SUCCESS">;
+        public SetJob(options: { GuildID?: string,  userId: string, Job: string}): Promise<'ALREADY_WORKING' | 'SUCCESS' | job[]>;
+        public Work(options: { GuildID?: string,  UserID: string}): Promise<wallet | { wallet: wallet, timeout: string, job: string, money: number }>;
         private msToTime(duration: number): string;
     }
 }
